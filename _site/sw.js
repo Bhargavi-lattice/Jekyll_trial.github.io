df95478aead8618f9b866e08e35019a83cac4ef4
Bhargavi-lattice <bhargavi@thelattice.in>
2023-12-27 17:11:14 +0530

    Commit

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1703676388';
+const cacheName = 'chirpy-1703677193';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
